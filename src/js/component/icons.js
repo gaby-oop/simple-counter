b48aac2b7cadbd7068a5dad53f4365dae3d07fa5
gaby-oop <gabysanz1505@gmail.com>
2021-02-09 05:04:48 +0000

    first commit

---
+++ src/js/component/icons.js
@@ -0,0 +1,9 @@
+//import @fontawesome a la libreria
+import fontawesome from "@fontawesome/fontawesome";
+import faClock from "@fontawesome/fontawesome-free-regular/faClock";
+ //llamamos a la fun
+fontawesome.conflig = {
+    autoReplaceSvg: "nest"
+};
+//
+ fontawesome.labrary.add(faClock);
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,51 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
+import PropTypes from "prop-types";
 //include bootstrap npm library into the bundle
 import "bootstrap";
 
-//include your index.scss file into the bundle
+//import your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+//creando un componente
+function SimpleCounter(props) {
+	return (
+		<div className="bigCounter">
+			<div className="calendar">{<i className="far fa-clock"></i>}</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitThree % 24}</div>
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+			<div className="two">{props.digitTwo % 60}</div>
+			<div className="one">{props.digitOne % 60}</div>
+		</div>
+	);
+}
+
+SimpleCounter.propTypes = {
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
+
+//establecer una fun.intervalo//setIntervalo// llama a una fun. en pocos segundos
+let counter = 0;
+setInterval(function() {
+	const four = Math.floor(counter / 6000);
+	const three = Math.floor(counter / 600);
+	const two = Math.floor(counter / 60);
+	const one = Math.floor(counter / 1);
+	// console.log(four, three, two, one);
+	counter++;
+ 
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
